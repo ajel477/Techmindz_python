88a47d2a0638370c468565bf99fc5ece5021cd3c
ajel477 <ajelmathew04@gmail.com>
2026-06-19 12:06:35 +0530

    lists

---
+++ nine.py
@@ -0,0 +1,49 @@
+# list 
+# collection of data 
+
+a = [2,4,1,5,3,1,3,5,2,4,5,2,6,2]
+
+# properties of lists
+
+#lists can have any element of any size
+
+data = [1,2.3, "mohan"]
+
+# lists are ordered
+# a = [1,2,3,4]
+# b = [4,3,2,1]
+
+# print(a == b)
+
+# lists are indexed
+
+a = [11,12,13,14,15,16]
+# print(a)
+# print(a[3])
+# print(a[2:5])
+# print(a[::-1])
+
+#     #012345678   
+# b = "mohan das"
+#          -2,-1   
+
+# lists are mutable (means the values inside lists can by changed or modified)
+# but strings are immutable
+
+# a = [11,12,13,14,15]
+# a[0] = 'mohan'
+# print(a)
+
+# lists are dynamic because they are mutable
+
+# lista are nested
+
+a = [11,12,[100,300],14,15]
+print(a[2][0])
+
+# or 
+
+b = a[2]
+print(b[0])
+
+
