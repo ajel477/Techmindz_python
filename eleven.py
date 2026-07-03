b19f7d283d805bac6687fc34f4e8829cae64e5af
ajel477 <ajelmathew04@gmail.com>
2026-07-03 16:37:43 +0530

    nested loops

---
+++ eleven.py
@@ -0,0 +1,38 @@
+# nested loops 
+#pattern printing practice
+
+# triangle shape pattern
+# for i in range(1, 6):
+#     for k in range(0, 6-i):
+#         print(" ", end="")
+#     for j in range(i):
+#         print("* ", end="")
+#     print()
+
+
+# # reversed triangle shape
+# for i in range(6, 1, -1):
+#     for k in range(6-i):
+#         print(" ", end="")
+#     for j in range(1, i):
+#         print("* ", end="")
+#     print()
+
+#print chessboard pattern
+
+# for i in range(1, 9):
+#     for j in range(1, 9):
+#         if (i+j) % 2 == 0:
+#             print("W", end=" ")
+#         else:
+#             print("T", end=" ")
+#     print()
+
+# H shape pattern printing
+        
+        
+
+
+
+
+
