2669ab553133bf7856989dd7bd107821430374ab
ajel477 <ajelmathew04@gmail.com>
2026-07-05 20:50:54 +0530

    recursions

---
+++ fourteen.py
@@ -0,0 +1,30 @@
+# higher order functions
+#when a function gets a function as its argument
+#Or when a function returns a function
+
+# Recursion
+
+# def hello():
+#     print("mohan")
+#     return hello()
+
+# hello()
+
+# print first 10 numbers
+
+# def counttoten(n):
+#     print(n)
+#     if n == 0:
+#         return 
+#     return counttoten(n-1)
+
+# counttoten(10)
+
+# to get the sum of first 10 numbers
+
+# def sumoften(n):
+#     if n == 0:
+#         return 0
+#     return n + sumoften(n-1)   #recursive call
+
+# print(sumoften(10))
