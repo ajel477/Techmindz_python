9c4ddb8c2c9f9b28fc48037fea159054e3e4674f
ajel477 <ajelmathew04@gmail.com>
2026-07-06 11:30:13 +0530

    scope of variable

---
+++ fifteen.py
@@ -0,0 +1,30 @@
+#scope of a variable
+# area in which it is recognised
+
+name ="ajel"
+
+# def myname():
+#     name = "jam"
+#     print(name)
+#     def nickname():
+#         name = "kj"
+#         print(name)
+#     nickname()
+
+# myname()
+
+# modules
+
+import math
+
+print(math.sqrt(25))
+print(math.factorial(5))
+print(math.pi)
+
+a = 6.3
+
+print(math.ceil(a))
+print(math.floor(a))
+
+
+
