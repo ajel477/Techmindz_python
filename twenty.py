006bddfaff381b83897f4d1547e99e6f2500e782
ajel477 <ajelmathew04@gmail.com>
2026-07-21 12:40:12 +0530

    sqlite connectivity, create, insert

--- twenty.py
+++ twenty.py
@@ -13,5 +13,36 @@ connector.execute(
 '''
 )
 
+def addStudent():
+    connectDB = sqlite3.connect("first.db") #establising a connection to the db
+    connector = connectDB.cursor()
+    student_name = input("enter student name :-")
+    student_age = int(input("enter student age:- "))
+    student_address = input("enter student address:- ")
+    connector.execute(
+    '''
+    INSERT INTO Student(name, age, address)
+        VALUES(?,?,?)
+    ''', (student_name, student_age, student_address))
+
+
+    connectDB.commit()
+    print("student added")
+
 connector.close()
+
+
+def main():
+    while True:
+        print("welcome t o student management system")
+        ch = int(input("1.ADD STUDENT\n2. VIEW STUDENT"))
+        if ch == 1:
+            addStudent()
+        else:
+            print("invalid choice")
+
+
+main()
+
+
 
